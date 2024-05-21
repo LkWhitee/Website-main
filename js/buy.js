64aad5d6a09ee0d33918eda082186a7d539f04e0
LookWhitee <pillon.tommaso@proton.me>
2024-05-21 20:20:45 +0200

    Vado via torno fra poco, fai il commit!!!

--- js/buy.js
+++ js/buy.js
@@ -6,6 +6,13 @@ let boolBuyPhase = true
  
 updateCount()
 
+function setCookie(cname, cvalue, exdays) { 
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+
 function onlick(){
     let buyPhase = document.getElementById("cartPopUp")
     if (boolBuyPhase){
